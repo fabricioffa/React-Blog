91127438304fe441589b3bbfa4400498c295de8e
Fabricio <fabricioffa08@gmail.com>
2021-11-19 12:01:23 -0300

    posts pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef } from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Posts } from './components/Posts';
@@ -5,18 +6,43 @@ import { getPostsAndPhotos } from './utils/getPostsAndPhotos';
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [allPosts, setAllposts] = useState([]);
-  const [page, setPage] = useState(0);
+  const [allPosts, setAllPosts] = useState([]);
+  const [lastPostIndex, setLastPostIndex] = useState(0);
   const [postsPerPage, setPostsPerPage] = useState(8);
 
+  const postsPerPageRef = useRef(postsPerPage);
+
+  const showMorePosts = useCallback(() => {
+    setPosts(allPosts.slice(0, lastPostIndex + postsPerPage));
+  }, [allPosts, lastPostIndex, postsPerPage]);
+
   useEffect(() => {
-    getPostsAndPhotos().then((data) => setPosts(data));
+    getPostsAndPhotos().then((data) => {
+      setAllPosts(data);
+      setPosts(data.slice(0, postsPerPageRef.current));
+    });
   }, []);
+
+  useEffect(() => {
+    showMorePosts();
+  }, [lastPostIndex, showMorePosts]);
+
+  useEffect(() => {
+    postsPerPageRef.current = postsPerPage;
+  }, [postsPerPage]);
 
   return (
     <section>
       <Posts posts={posts} />
-      <button>Show more posts</button>
+      <button
+        className="show-more-posts"
+        onClick={() => {
+          setLastPostIndex((prevIndex) => prevIndex + postsPerPage);
+          showMorePosts();
+        }}
+      >
+        Show more posts
+      </button>
     </section>
   );
 }
--- src/components/PostContent/index.jsx
+++ src/components/PostContent/index.jsx
@@ -4,7 +4,9 @@ export const PostContent = ({ post }) => {
   return (
     <div className="post-content">
       <img src={post.photo} alt="" />
-      <h4>{post.title}</h4>
+      <h4>
+        {post.title} {post.id}
+      </h4>
       <p>{post.body}</p>
     </div>
   );
